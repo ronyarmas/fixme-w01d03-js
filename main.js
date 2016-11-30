b9c2f08eeb9bfde6191adcbcd140ad41b47610e0
Rony Armas <rony.lunaset@gmail.com>
2016-11-30 14:54:46 -0800

    js debug exercise

--- main.js
+++ main.js
@@ -2,22 +2,22 @@
 // When it works it should render:
 // User: Neil Armstrong has been to 3 places
 
-var user {
+var user = {
   name: {
-    first: 'Neil';
-    last 'Armstrong'
-  }
-  bornOn: new Date('December 12, 1979'),
+    first: 'Neil',
+    last: 'Armstrong'
+  },
+  bornOn: new Date("December 12 1979")
 };
 
-user[occupiation] = 'spaceman';
+user['occupation'] = 'spaceman';
 
 var destinations = [];
 
-destinations.push('Moon', 'Mars 'Jupiter');
+destinations.push('Moon', 'Mars', 'Jupiter');
 
 user.destinations = destinations;
 
-message = 'User: ' + User.name.first + ' ' + user.last + ' has been to ' + user.destination.length + ' places';
+var message = 'User: ' + user.name.first + ' ' + user.name.last + ' has been to ' + destinations.length + ' places';
 
 document.write(message);
